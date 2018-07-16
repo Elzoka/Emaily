3288784b517e8248c5452b63f75b86c8e50ddd81
mahmoud elzoka <mahmoud_elzoka@yahoo.com>
2018-07-16 16:35:21 +0200

    Setting up passport with google ouath API

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const {googleClientID, googleClientSecret} = require("./config/keys");
+
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy({
+        clientID: googleClientID,
+        clientSecret: googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log("access token", accessToken);
+        console.log("refresh token", refreshToken);
+        console.log("profile", profile);
+    })
+);
 
 app.get('/', (req, res) => {
     res.send({hi: 'There'});
 });
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 
