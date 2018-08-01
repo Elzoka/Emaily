14a0583bb605503d41f527dd51f9a6056083e107
mahmoud elzoka <mahmoud_elzoka@yahoo.com>
2018-08-01 21:15:14 +0200

    Setup react-router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return(
-        <div>Hi there</div>
+        <div>
+            <Header />
+            <Router>
+                <Switch>
+                    <Route path='/' component={Landing} exact />
+                    <Route path='/surveys/new' component={SurveyNew} />
+                    <Route path='/surveys' component={Dashboard} />
+                </Switch>
+            </Router>
+        </div>
     );
 };
 
