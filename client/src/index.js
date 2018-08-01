9b075576b9076cae72602078b7de89e81014e9d5
mahmoud elzoka <mahmoud_elzoka@yahoo.com>
2018-08-02 01:06:21 +0200

    Add redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 import 'materialize-css/dist/css/materialize.min.css';
 
 import App from './components/App'
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
