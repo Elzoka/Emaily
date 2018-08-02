12c193ea796a4f844a8b8620631f57153831dede
mahmoud elzoka <mahmoud_elzoka@yahoo.com>
2018-08-02 05:48:34 +0200

    setting up authReducers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {connect} from 'react-redux'
+
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-    return(
-        <div className='container'>
-            <Header />
-            <Router>
-                <Switch>
-                    <Route path='/' component={Landing} exact />
-                    <Route path='/surveys/new' component={SurveyNew} />
-                    <Route path='/surveys' component={Dashboard} />
-                </Switch>
-            </Router>
-        </div>
-    );
+class App extends Component {
+    componentDidMount(){
+        this.props.fetchUser();
+    }
+    render(){
+        return(
+            <div className='container'>
+                <Header />
+                <Router>
+                    <Switch>
+                        <Route path='/' component={Landing} exact />
+                        <Route path='/surveys/new' component={SurveyNew} />
+                        <Route path='/surveys' component={Dashboard} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 };
 
-export default App;
+export default connect(null, actions)(App);
